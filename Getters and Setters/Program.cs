3e3a3d5695f97ba06f3b038eb314f2a1641f9944
Lenovo-User <Lenovo-User@DESKTOP-RNPFAS5>
2023-10-11 09:39:22 +0200

    Add project files.

---
+++ Getters and Setters/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Getters_and_Setters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Movie movie1 = new Movie("Titanic", "Jim", "PG-13");
+            Movie movie2 = new Movie("Unreleased", "Kim", "PG");
+
+            Console.WriteLine(movie1.Rating);
+
+            // Console.ReadLine(); // You may choose to uncomment this if you want to pause the console before it closes.
+        }
+    }
+}
